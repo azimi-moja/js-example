ccdab52c9607f31f909924d85ce6216b920942bf
Moja Azimi Vaghar <azimi_moja@yahoo.com>
2021-07-20 13:26:56 +0430

    add variable function and clean code

--- function/script.js
+++ function/script.js
@@ -1,13 +1,29 @@
+//Define Functions
 function sum(a, b){
     console.log("sum = " + (a + b));
 }
 
 function sayHello(){
-    console.log("Say Hello!!");
+    console.log("Hello!!");
 }
 
-//call First function
+var goodbyeFunc = function sayGoogbye(){
+    console.log("Goodbye!!");
+}
+
+var devideFunc = function devide(a, b){
+    console.log("device = " + (a / b));
+}
+
+//call SayHello function
 sayHello();
 
 //call sum function
 sum(5, 10);
+
+//call devide function
+devideFunc(5, 10);
+
+//call Goodbye function
+goodbyeFunc();
+
