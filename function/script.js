90e713434a459e309a30bb51b05ae90cfeecc8d4
Moja Azimi Vaghar <azimi_moja@yahoo.com>
2021-07-20 12:27:18 +0430

    js 5 function

---
+++ function/script.js
@@ -0,0 +1,13 @@
+function sum(a, b){
+    console.log("sum = " + (a + b));
+}
+
+function sayHello(){
+    console.log("Say Hello!!");
+}
+
+//call First function
+sayHello();
+
+//call sum function
+sum(5, 10);
