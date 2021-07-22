4310f311b2bdd21cb0ccaa3a27b3dccd2cfae74f
Moja Azimi Vaghar <azimi_moja@yahoo.com>
2021-07-22 12:28:09 +0430

    add event listner

--- event/script.js
+++ event/script.js
@@ -1,6 +1,9 @@
 'use strict'
 
-var title = document.querySelector('#title')
+var title = document.querySelector('#title');
+var el1 = document.querySelector('.el1');
+var el2 = document.querySelector('.el2');
+var link = document.querySelector('#link');
 
 title.onmouseenter = function(e) {
     e.target.style.backgroundColor = 'red';
@@ -9,3 +12,30 @@ title.onmouseenter = function(e) {
 title.onmouseout = function(e) {
     e.target.style.backgroundColor = 'yellow';
 }
+
+var eventOne = function(e){
+    console.log("click event ONE");
+}
+
+var eventTwo = function(e){
+    e.stopPropagation();
+    console.log("click event TWO");
+}
+
+var eventThree = function(e){
+    console.log("click event THREE");
+}
+
+var eventLink = function(e){
+    e.preventDefault();
+    console.log("click event Link");
+}
+
+el1.addEventListener('click', eventOne);
+el2.addEventListener('click', eventTwo);
+el1.addEventListener('click', eventThree);
+
+link.addEventListener('click', eventLink);
+
+el1.removeEventListener(eventThree);
+
